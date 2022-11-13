518714af57eba83cb02a85233254b2819b59dce0
Kim Myungseong <forwarm5891@gmail.com>
2022-11-13 21:36:45 +0900

    notFoundException과 HttpError file

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,21 +1,34 @@
-import { Controller, Get, Post } from '@nestjs/common';
-
-
+import { Controller, Get, Post, Body, Param, NotFoundException } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 @Controller('messages')
 export class MessagesController {
 
+  messagesService:MessagesService;
+
+  constructor() {
+    // DONT DO THIS ON REAL APP
+    // USE DEPENDENCY INJECTION
+    this.messagesService = new MessagesService;
+  }
+
   @Get()
   listMessages() {
-    return 'this is messages'
+    return this.messagesService.findAll();
   }
 
   @Post()
-  createMessage() {
-
+  createMessage(@Body() body: CreateMessageDto) {
+    return this.messagesService.create(body.content)
   }
 
   @Get('/:id')
-  getMessage() {
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
 
+    if(!message){
+      throw new NotFoundException('해당 id의 메세지는 존재하지 않습니다.');
+    }
+    return message
   }
 }
